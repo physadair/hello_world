87c2e3556454b5cda3bbead346b63fd61ce35a99
葉永順 <physadair>
2019-04-21 16:35:40 +0800

    Update  FindMedianSortedArrays.java

--- java/FindMedianSortedArrays.java
+++ java/FindMedianSortedArrays.java
@@ -35,6 +35,33 @@ public class FindMedianSortedArrays
         }
     }
     
+    public static int getKth(int[] nums1, int[] nums2, int k)
+    {
+        if (nums1.length > nums2.length) 
+            return getKth(nums2, nums1, k);
+
+        if (nums1.length == 0) 
+            return nums2[k-1];
+
+        if (k == 1)
+            return Math.min(nums1[0], nums2[0]);
+
+        int i = Math.min(nums1.length, k/2);
+        int j = Math.min(nums2.length, k/2);
+        if (nums1[i-1] < nums2[j-1])
+            return getKth(Arrays.copyOfRange(nums1, i, nums1.length), nums2, k-i);
+        else
+            return getKth(nums1, Arrays.copyOfRange(nums2, j, nums2.length),  k-j);
+    }
+
+    public static double findMedianSortedArrays_v2(int[] nums1, int[] nums2)
+    {
+        int l = (nums1.length + nums2.length + 1)/2;
+        int r = (nums1.length + nums2.length + 2)/2;
+
+        return (getKth(nums1, nums2, l) + getKth(nums1, nums2, r))/2.;
+    }
+    
 
     public static void main(String[] args) 
     {
