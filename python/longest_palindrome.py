626b8a820f4771fd9d00bff511049938d66722f7
葉永順 <physadair>
2019-06-27 23:19:53 +0800

    longest_palindrome.py

--- python/longest_palindrome.py
+++ python/longest_palindrome.py
@@ -2,20 +2,23 @@
 
 class Solution(object):
     def longestPalindrome(self, s):
-        pass
+        max_palindrome = ""
+        for i in range(len(s)):
+            # odd 
+            for t in range(2):
+                j = 0
+                while i+j+t<len(s) and i-j>=0:
+                    if (s[i-j] == s[i+j+t]):
+                        if len(s[i-j:i+j+t+1]) > len(max_palindrome):
+                            max_palindrome = s[i-j:i+j+t+1]
+                    else:
+                        break
+                    j += 1
         
-
-    def isPalindrome(self, s):
-        n = len(s)
-        for i in range(n//2):
-            print(i, n-i-1, s[i], s[n-i-1])
-            if s[i] != s[n-i-1]: return False
-        return True
+        return max_palindrome
 
 if __name__ == "__main__":
-    s1 = "abcdedcba"
-    s2 = "abcdefg"
-
-    print(Solution().isPalindrome(s2))
-    print(Solution().isPalindrome(s1))
-
+    print(Solution().longestPalindrome('adddaabcdefghgfedcbazz'))
+    print(Solution().longestPalindrome('cbbd'))
+    print(Solution().longestPalindrome('c'))
+                
